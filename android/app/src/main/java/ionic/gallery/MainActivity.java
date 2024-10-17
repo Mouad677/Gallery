38f01918f045b36193b4ff08c6778b840158cde8
Mouad677 <drissimouad54@gmail.com>
2024-10-17 17:59:25 +0100

    Initial commit

---
+++ android/app/src/main/java/ionic/gallery/MainActivity.java
@@ -0,0 +1,5 @@
+package ionic.gallery;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ src/pages/Tab2.tsx
@@ -0,0 +1,55 @@
+import { 
+  IonContent, 
+  IonHeader, 
+  IonPage, 
+  IonTitle, 
+  IonToolbar, 
+  IonFab,
+  IonFabButton,
+  IonIcon,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonImg,
+  IonActionSheet, } from '@ionic/react';
+import ExploreContainer from '../components/ExploreContainer';
+import './Tab2.css';
+import { camera, trash, close } from 'ionicons/icons';
+import { usePhotoGallery } from '../hooks/usePhotoGallery';
+
+const Tab2: React.FC = () => {
+  const { photos, takePhoto } = usePhotoGallery();
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Gallery</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Gallery</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonGrid>
+          <IonRow>
+            {photos.map((photo, index) => (
+              <IonCol size="6" key={photo.filepath}>
+                <IonImg src={photo.webviewPath} />
+              </IonCol>
+        ))}
+          </IonRow>
+        </IonGrid>
+        <ExploreContainer name="" />
+        <IonFab vertical="bottom" horizontal="center" slot="fixed">
+    <IonFabButton onClick={() => takePhoto()}>
+      <IonIcon icon={camera}></IonIcon>
+    </IonFabButton>
+  </IonFab>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Tab2;
