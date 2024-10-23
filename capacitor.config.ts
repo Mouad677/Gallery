d319536ae4b06f91529ad572c798b16fa5dcf2d7
Mouad677 <drissimouad54@gmail.com>
2024-10-23 19:34:00 +0100

    Live Updates Configuration

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,16 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'ionic.gallery',
   appName: 'gallery',
-  webDir: 'dist'
+  webDir: 'dist',
+
+  plugins: {
+    LiveUpdates: {
+      appId: 'ff4aeb1f',
+      channel: 'Production',
+      autoUpdateMethod: 'background',
+      maxVersions: 2
+    }
+  }
 };
 
 export default config;
